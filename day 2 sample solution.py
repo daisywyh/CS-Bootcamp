4da6fbc860d23a0cb92c711cb33408dc1b3d2b2d
wangyhdaisy <59313915+wangyhdaisy@users.noreply.github.com>
2020-06-16 22:14:22 +0800

    Sample solution to the number guesser game

--- day 2 sample solution.py
+++ day 2 sample solution.py
@@ -2,21 +2,25 @@ import random
 #import the random library
 score = 0
 #define score, set to 0
+
 for i in range(10):
 #for loop with iterator i, allows us to repeat the questions 10 times
     num1 = random.randint(1,10)
     num2 = random.randint(1,10)
     #set the two numbers
+    
     print('Question',i+1, ':\n', num1, '+', num2, '=?')
     #print the question
     #note the use of \n to make a new line
     ans = int(input())
     #set answer equal to user input
+    
     if ans == num1 + num2:
     #if the user answers correctly
         print("Correct!")
         score = score + 1
         #tell the user that they're correct and increase score by 1
+        
     else:
         print("Incorrect :(")
         score = score - 1
@@ -27,5 +31,3 @@ for i in range(10):
 
 print('\nYour final score is:', score, '\n')
 #print the user's final score after each question
-
-
